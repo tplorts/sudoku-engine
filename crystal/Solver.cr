5b56ae744286b017b1613462d04a12665410a92a
Ted Lorts <tplorts@gmail.com>
2019-04-11 21:02:30 -0700

    🔮 🎊 crystal solution solves its first medium! 😁 🕺

--- crystal/Solver.cr
+++ crystal/Solver.cr
@@ -15,8 +15,12 @@ module Sudoku
       @sudoku.load_from_file(filename)
     end
 
+    def debug(stuff)
+      puts stuff if @debug_output_enabled
+    end
+
     def print_grid
-      puts @sudoku.to_s if @debug_output_enabled
+      debug @sudoku.to_s
     end
 
     def empty_count
@@ -42,22 +46,36 @@ module Sudoku
       until complete? || placements_made == 0
         initial_empty_count = empty_count
 
-        exhaustively_fill_determined_cells
+        solve_determined
+        eliminate_candidates_by_partially_determined unless complete?
 
         placements_made = initial_empty_count - empty_count
-
-        validate
-
-        # break if empty_count == 0
-        # (1..N).each do |n|
-        #   place_throughout(n)
-        #   break if empty_count == 0
-        # end
-
+        debug "major iteration #{iterations}: #{placements_made} placements"
         iterations += 1
       end
 
       raise "Failed to solve" if empty_count > 0
+    end
+
+    def solve_determined
+      iterations = 0
+      placements_made = nil
+
+      until complete? || placements_made == 0
+        initial_empty_count = empty_count
+
+        exhaustively_fill_determined_cells
+
+        validate
+
+        exhaustively_fill_determined_positions
+
+        validate
+
+        placements_made = initial_empty_count - empty_count
+        debug "determined iteration #{iterations}: #{placements_made} placements"
+        iterations += 1
+      end
     end
 
     # Repeatedly searches for cells which are fully determined by their own
@@ -74,8 +92,8 @@ module Sudoku
 
         placements_made = initial_empty_count - empty_count
 
-        puts "Filled #{placements_made} cells" if @debug_output_enabled
-        print_grid
+        debug "Filled #{placements_made} cells" if @debug_output_enabled
+        print_grid if placements_made > 0
         validate
       end
     end
@@ -86,12 +104,30 @@ module Sudoku
       end
     end
 
-    # Searches for blocks in which there is only one candidate position for value
-    # and places value there if any such fully determined placements are found.
-    def place_throughout(value : Int)
-      @sudoku.each_block do |block|
-        candidate_positions = find_candidate_positions(value, block)
-        @sudoku.place(value, candidate_positions[0]) if candidate_positions.size == 1
+    def exhaustively_fill_determined_positions
+      placements_made = nil
+
+      until complete? || placements_made == 0
+        initial_empty_count = empty_count
+
+        fill_determined_positions
+
+        placements_made = initial_empty_count - empty_count
+
+        debug "Filled #{placements_made} positions" if @debug_output_enabled
+        print_grid if placements_made > 0
+        validate
+      end
+    end
+
+    # Searches for blocks in which there is only one candidate position for a value
+    # and places the value there if any such fully determined positions are found.
+    def fill_determined_positions
+      (1..N).each do |value|
+        @sudoku.each_block do |block|
+          candidate_positions = find_candidate_positions(value, block)
+          @sudoku.place(value, candidate_positions[0]) if candidate_positions.size == 1
+        end
       end
     end
 
@@ -113,6 +149,74 @@ module Sudoku
       end
 
       return candidates
+    end
+
+    def eliminate_candidates_by_partially_determined
+      @sudoku.each_block do |block|
+        (1..N).each do |value|
+          # only consider values not yet placed in this block
+          next if block.has?(value)
+
+          candidate_rows = Set(Int32).new
+          candidate_columns = Set(Int32).new
+
+          # Find the set of rows/columns within this block which could contain
+          # value.
+          block.each_cell_with_position do |cell, (row_index, column_index)|
+            if cell.candidate?(value)
+              candidate_rows.add(row_index)
+              candidate_columns.add(column_index)
+            end
+          end
+
+          # And now, if there is only one row that could contain value in this
+          # block, then we can deduce that value cannot be placed in that row
+          # in the two blocks horizontally adjacent to this block.
+          # And likewise, for columns and vertically adjacent blocks.
+          row_determined = candidate_rows.size == 1
+          column_determined = candidate_columns.size == 1
+
+          determined_row_index = candidate_rows.to_a[0]
+          determined_column_index = candidate_columns.to_a[0]
+
+          if row_determined && column_determined
+            @sudoku.place(value, {determined_row_index, determined_column_index})
+            next # next value in this block
+          end
+
+          if row_determined
+            row = @sudoku.row(determined_row_index)
+
+            debug "Eliminating #{value} from row #{determined_row_index} except in columns #{block.column_range}"
+
+            # Since value will definitely go in this row within this block,
+            # the two horizontally adjacent blocks can eliminate value as a
+            # candidate from all of their cells in this row.
+            row.each_cell_with_position do |cell, (_, column_index)|
+              unless block.column_range.includes?(column_index)
+                debug "   > eliminate #{value} from (#{determined_row_index},#{column_index})" if cell.candidate?(value)
+              end
+              cell.eliminate_candidate(value) unless block.column_range.includes?(column_index)
+            end
+          end
+
+          if column_determined
+            column = @sudoku.column(determined_column_index)
+
+            debug "Eliminating #{value} from column #{determined_column_index} except in rows #{block.row_range}"
+
+            # Since value will definitely go in this column within this block,
+            # the two vertically adjacent blocks can eliminate value as a
+            # candidate from all of their cells in this column.
+            column.each_cell_with_position do |cell, (row_index)|
+              unless block.row_range.includes?(row_index)
+                debug "   > eliminate #{value} from (#{row_index},#{determined_column_index})" if cell.candidate?(value)
+              end
+              cell.eliminate_candidate(value) unless block.row_range.includes?(row_index)
+            end
+          end
+        end
+      end
     end
   end
 end
