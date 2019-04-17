58253919808c644d97ce1d0004af6a91ca36e995
Ted Lorts <tplorts@gmail.com>
2019-04-17 13:13:46 -0700

    just format

--- crystal/main.cr
+++ crystal/main.cr
@@ -1,6 +1,10 @@
 require "./Solver"
 
-solver = Sudoku::Solver.new("../puzzles/20190220b.sudoku", debug_output_enabled: true, validation_enabled: true)
+solver = Sudoku::Solver.new(
+  "../puzzles/20190220b.sudoku",
+  debug_output_enabled: true,
+  validation_enabled: true
+)
 
 puts "Before"
 solver.print_grid
